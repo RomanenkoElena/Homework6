2e201bbc9148babea5ad0afeb0efdc2fd6fd92cd
Elena Romanenko <murenec_elena@mail.ru>
2022-11-07 00:15:25 +0600

    первый вариант решения

--- ex41/Program.cs
+++ ex41/Program.cs
@@ -1,1 +1,34 @@
 ﻿// 41. Пользователь вводит с клавиатуры М чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// Пример: 0,7,8,-2,-2 -> 2
+
+Console.Write("Введите числа через запятую: ");
+int[] GetArray = Array.ConvertAll(Console.ReadLine().Split(","), int.Parse);
+int result = SearchPositive(GetArray);
+Console.Write("Получен ряд чисел:"); 
+PrintArray(GetArray); // вывод на экран полученного массива чисел
+Console.WriteLine($"Кол-во элементов больше нуля: {result}");
+
+int SearchPositive(int[] array) // метод нахождения кол-ва полож.чисел
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+void PrintArray(int[] num) // метод для вывода массива на экран
+{
+    int pos = 0;
+    while (pos < num.Length)
+    {
+        Console.Write($"{num[pos]} ");
+        pos++;
+    }
+    Console.WriteLine();
+}
